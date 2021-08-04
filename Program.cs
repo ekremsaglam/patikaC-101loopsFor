20104e552ce0358a1af4ebc8268083347d620bb2
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-04 08:36:31 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace loopsForBreakContinue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number : ");
+            int sayac=int.Parse(Console.ReadLine());
+
+            for (int i = 1 ; i<= sayac; i++){
+                if(i%2 ==1){
+                    Console.WriteLine(i);
+                }
+            }
+
+            int tekToplam = 0;
+            int ciftToplam = 0 ;
+            for(int i = 1 ; i<=1000; i++){
+                if(i%2 ==1){
+                    tekToplam+=i;
+
+                }
+                else{
+                    ciftToplam+=i;
+                }
+
+            }
+            Console.WriteLine("tekToplam : " + tekToplam);
+            Console.WriteLine("Çift Toplam : " + ciftToplam );
+
+
+            for (int i =1 ; i<10 ; i++){
+                if(i==4)
+                    continue;
+                    //break;
+                Console.WriteLine(i);
+            }
         }
     }
 }
